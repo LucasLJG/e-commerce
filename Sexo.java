9b5d4d748fa88b6f5c31a237986d24f461305562
czariv <68134220+czariv@users.noreply.github.com>
2020-12-07 20:21:54 -0300

    Variáveis não estavam como finais

--- Sexo.java
+++ Sexo.java
@@ -1,29 +1,20 @@
 
 public enum Sexo {
-	HOMEM ("M","Masculino"),
-	MULHER ("F","Feminino");
+	HOMEM("M","Masculino"),
+	MULHER("F","Feminino");
 	
-	private String sigla;
-	private String descricao;
+	private final String sigla;
+	private final String descricao;
 	
-	Sexo(String sigla, String descricao){
+	Sexo(String sigla, String descricao) {
 		this.sigla = sigla;
 		this.descricao = descricao;
 	}
-	
+
 	public String getSigla() {
 		return sigla;
 	}
-	
-	public void setSigla(String sigla) {
-		this.sigla = sigla;
-	}
-	
 	public String getDescricao() {
 		return descricao;
-	}
-	
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
 	}
 }
