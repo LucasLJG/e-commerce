813190a90877d47449de30eb1f2f829e7c5b8ef0
andre-portella <75337495+andre-portella@users.noreply.github.com>
2020-12-07 22:12:07 -0300

    Data de nascimento

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
+		Data dataNascimento;
+		System.out.println("Informe sua data de nascimento (dd/mm/aaaa");
+		String data_nasc [] = leitor.nextLine().split("/"); 
+		int dia = Integer.parseInt(data_nasc[0]);
+		int mes = Integer.parseInt(data_nasc[1]);
+		int ano = Integer.parseInt(data_nasc[2]);
+		
+		dataNascimento(dia, mes, ano);
 
 	}
 
