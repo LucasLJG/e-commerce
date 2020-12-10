fb3a45c587c3a4e676d33beca841157b84036d5e
andre-portella <75337495+andre-portella@users.noreply.github.com>
2020-12-09 21:43:42 -0300

    Instanciação de objetos.

--- Main.java
+++ Main.java
@@ -6,10 +6,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		/* 
+		* Instanciando itens
+		*/
+		Item celular = new Item("Celular", 64368, 1, 1800.00f, 25, true);
+		Item notebook = new Item("Notebook", 58794, 1, 3200.00f, 15, true);
+		Item camera_fotografica = new Item("Camera Fotografica", 74185, 1, 1500.00f, 10, true);
+		Item oculos = new Item("Óculos", 17463, 2, 300.50f, 50, true);
+		Item impressora = new Item("Impressora", 87465, 1, 700.00f, 8, true);
+		Item lapis = new Item("Lápis", 78549, 5, 1.15f, 1000, true);
+		Item caderno = new Item("Caderno", 41208, 2, 8.50f, 250, true);
+		Item caneta = new Item ("Caneta", 58971, 3, 1.75f, 850, true);
+		Item agenda = new Item("Agenda", 37148, 1, 11.75f, 150, true);
+		
+		
 		Scanner leitor = new Scanner(System.in);
 		
 		Perfil perfil_admin1 = new Perfil(Sexo.HOMEM, new GregorianCalendar(2020, 12, 8), Estado.MINAS_GERAIS.getCapital(), Estado.MINAS_GERAIS, "15 88888-8888", "123XXXXXXXXX/89");
-		Administrador admin1 = new Administrador(2, "Rafael Santos", "rafael_santos@mail.com", "12345678", perfil_admin1);
+		Administrador admin1 = new Administrador(2, "Rafael Santos", "rafael_santos@mail.com", "12345678", perfil_admin1, true);
 		
 		System.out.println("Bem vindo. Gostaria de se cadastrar? (S/N)");
 		String cadastro = leitor.nextLine();
@@ -41,40 +55,36 @@ public class Main {
 			int ano = Integer.parseInt(dataNascimento[2]);
 			
 			Perfil perfil_user1 = new Perfil(sexo, new GregorianCalendar(ano, mes, dia), Estado.SÃO_PAULO.getCapital(), Estado.SÃO_PAULO, "19 99999-9999", "257XXXXXXXXX/55");		
-			Usuario user1 = new Usuario(1, nome, email, senha, perfil_user1);
+			Usuario user1 = new Usuario(1, nome, email, senha, perfil_user1, true);
 			
 			
 			//criar cadastro de user1
 			admin1.criarCadastro(user1);
 			
 			Pedido pedido1_user1 = new Pedido(1, "Carlos", new GregorianCalendar(2020, 12, 5), true);
-			
 			user1.getPedidos().add(pedido1_user1);
 			
-			/* 
-			 * Instanciando itens
-			 */
-			
-			Item celular = new Item("Celular", 64368, 1, 1800.00f, 25, true);
-			Item notebook = new Item("Notebook", 58794, 1, 3200.00f, 15, true);
-			Item camera_fotografica = new Item("Camera Fotografica", 74185, 1, 1500.00f, 10, true);
-			Item oculos = new Item("Óculos", 17463, 2, 300.50f, 50, true);
-			Item impressora = new Item("Impressora", 87465, 1, 700.00f, 8, true);
-			Item lapis = new Item("Lápis", 78549, 5, 1.15f, 1000, true);
-			Item caderno = new Item("Caderno", 41208, 2, 8.50f, 250, true);
-			Item caneta = new Item ("Caneta", 58971, 3, 1.75f, 850, true);
-			Item agenda = new Item("Agenda", 37148, 1, 11.75f, 150, true);
-			
-	
 			pedido1_user1.getItem().add(celular);
 			pedido1_user1.getItem().add(oculos);
 			pedido1_user1.getItem().add(agenda);
 			
+			//novo pedido de user1
+			Pedido pedido2_user1 = new Pedido(3, "Antonio", new GregorianCalendar(2020, 12, 8), true);
+			user1.getPedidos().add(pedido2_user1);
+			pedido2_user1.getItem().add(caneta);
+			pedido2_user1.getItem().add(caderno);
 			
+			System.out.printf("\n\nDados de %s\n", admin1.getCadastroUsuarios().getUsuariosCadastrados().get(0).getNome());
+			System.out.println(admin1.getCadastroUsuarios().getUsuariosCadastrados().get(0));
+			System.out.println(admin1.getCadastroUsuarios().getUsuariosCadastrados().get(0).getPerfil());
+		}
+		else {
+			System.out.println("Usuario nao deseja se cadastrar\n");
+		}
 			
-			
+
 			Perfil perfil_user2 = new Perfil(Sexo.MULHER, new GregorianCalendar(1995, 11, 10), Estado.ALAGOAS.getCapital(), Estado.ALAGOAS, "82 88888-8888", "742XXXXXXXXX/35");		
-			Usuario user2 = new Usuario(1, "Giovanna Nogueira", "giovanna.nogueira@email.com", "giovanna1234", perfil_user2);
+			Usuario user2 = new Usuario(1, "Giovanna Nogueira", "giovanna.nogueira@email.com", "giovanna1234", perfil_user2, true);
 			admin1.criarCadastro(user2);
 			
 			Pedido pedido1_user2 = new Pedido(2, "Antonio", new GregorianCalendar(2020, 12, 7), true);
@@ -86,17 +96,7 @@ public class Main {
 			
 			
 			
-			//Criando novos pedidos para user1 e user2
-			
-			Pedido pedido2_user1 = new Pedido(3, "Antonio", new GregorianCalendar(2020, 12, 8), true);
-			user1.getPedidos().add(pedido2_user1);
-			pedido2_user1.getItem().add(caneta);
-			pedido2_user1.getItem().add(caderno);
-			
-			System.out.printf("\n\nDados de %s\n", user1.getNome());
-			System.out.println(user1);
-			System.out.println(perfil_user1);
-			
+			//Criando novos pedidos para user2
 			
 			
 			Pedido pedido2_user2 = new Pedido(4, "Carlos", new GregorianCalendar(2020, 12, 8), true);
@@ -109,22 +109,16 @@ public class Main {
 			
 			
 			//remover usuario
-			admin1.removerCadastro(user1);
+			admin1.removerCadastro(user2);
 			System.out.println(admin1.getCadastroUsuarios());
 			
 			
 			
 			//alterar senha
 			user2.alterar_senha("giovanna.nogueira@email.com", "giovanna1234");
-			System.out.printf("Dados de %s\n", user2.getNome());
-			System.out.println(user2);
-			
-			
+
 			leitor.close();
-		}
-		else {
-			System.out.println("Usuario nao deseja se cadastrar");
-		}
+
 	}
 }
 
