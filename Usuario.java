c1d81965e904273e150e7cfe2ac7304287f36caf
andre-portella <75337495+andre-portella@users.noreply.github.com>
2020-12-09 21:45:58 -0300

    Adicionado parâmetro status e métodos

--- Usuario.java
+++ Usuario.java
@@ -7,13 +7,15 @@ public class Usuario {
 	private String senha;
 	private ArrayList<Pedido> pedidos;
 	private Perfil perfil;
+	private boolean status;
 	
-	public Usuario(int identificador, String nome, String email, String senha, Perfil perfil) {
+	public Usuario(int identificador, String nome, String email, String senha, Perfil perfil, boolean status) {
 		this.identificador = identificador;
 		this.nome = nome;
 		this.email = email;
 		this.senha = senha;
 		this.perfil = perfil;
+		this.status = status;
 		pedidos = new ArrayList<Pedido>();
 	}
 
@@ -61,47 +63,69 @@ public class Usuario {
 		this.perfil = perfil;
 	}
 	
+	public boolean getStatus() {
+		return status;
+	}
+	
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
 	public void recuperar_senha(String email) {
-		if (this.email == email) {
-			System.out.println("Digite o número do seu cpf para confirmação de autenticidade: \n");
-			Scanner dados = new Scanner(System.in);
-			String entrada = dados.nextLine();
-			dados.close();
-			if (this.perfil.getCpf().equals(entrada) == true) {
-				System.out.println("Senha: ");
-				System.out.println(this.getSenha());
+		
+		if(status == true) {
+		
+			if (this.email == email) {
+				System.out.println("Digite o numero do seu cpf para confirmacao de autenticidade: \n");
+				Scanner dados = new Scanner(System.in);
+				String entrada = dados.nextLine();
+				dados.close();
+				if (this.perfil.getCpf().equals(entrada) == true) {
+					System.out.println("Senha: ");
+					System.out.println(this.getSenha());
+				}
+				else {
+					System.out.println("Numero de CPF invÃ¡lido.\n");
+				}
 			}
-			else {
-				System.out.println("Número de CPF inválido.\n");
-			}
+			System.out.println("Endereco de e-mail invalido.\n");
 		}
-		System.out.println("Endereço de e-mail inválido.\n");
+		else {
+			System.out.println("Usuário não cadastrado.\n");
+		}
 	}
 	
 	public void alterar_senha(String email, String senha) {
-		if (this.getEmail().equals(email)== true && this.getSenha().equals(senha) == true) {
-			System.out.println("Digite a nova senha: \n");
-			Scanner senha_nova = new Scanner(System.in);
-			String senha_atual = senha_nova.nextLine();
-			senha_nova.close();
-			this.setSenha(senha_atual);
-			System.out.println("Senha alterada com sucesso.\n");
+		
+		if(status == true) {
+			
+			if (this.getEmail().equals(email)== true && this.getSenha().equals(senha) == true) {
+				System.out.printf("Digite a nova senha de %s: \n", this.getNome());
+				Scanner senha_nova = new Scanner(System.in);
+				String senha_atual = senha_nova.nextLine();
+				senha_nova.close();
+				this.setSenha(senha_atual);
+				System.out.println("Senha alterada com sucesso.\n");
+			}
+			else {
+				System.out.println("Dados invalidos.\n");
+			}
 		}
 		else {
-			System.out.println("Dados inválidos.\n");
+			System.out.println("Usuário não cadastrado");
 		}
 	}
 	
 	@Override
 	public String toString() {
 		String out = "";
-		out = out + "Identificação do usuário: " + getIdentificador() + "\n";
+		out = out + "Identificacao do usuario: " + getIdentificador() + "\n";
 		out = out + "Nome: " + getNome() + "\n";
 		if (this.pedidos.isEmpty()) {
-			out = out + "O usuário não tem pedidos realizados.\n";
+			out = out + "O usuario nao tem pedidos realizados.\n";
 		}
 		else {
-			out = out + "Pedido do Usuário: \n";
+			out = out + "Pedido do Usuario: \n";
 			int j = 1;
 			for (Pedido pedido_atual: getPedidos()) {
 				for (int i = 0; i < pedido_atual.getItem().size(); i++) {
@@ -114,6 +138,5 @@ public class Usuario {
 		}
 		return out;
 	}
-	
 	
 }
