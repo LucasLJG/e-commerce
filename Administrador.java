7dd75ac8411354474bdedd6b134e756f3c30bb84
andre-portella <75337495+andre-portella@users.noreply.github.com>
2020-12-09 21:44:46 -0300

    Alterar status de usuario_remover

--- Administrador.java
+++ Administrador.java
@@ -2,8 +2,8 @@ public class Administrador extends Usuario{
 	private UsuariosCadastrados cadastroUsuarios;
 	private static int quantidadeUsuarios = 0;
 	
-	public Administrador(int identificador, String nome, String email, String senha, Perfil perfil) {
-		super(identificador, nome, email, senha, perfil);
+	public Administrador(int identificador, String nome, String email, String senha, Perfil perfil, boolean status) {
+		super(identificador, nome, email, senha, perfil, status);
 		cadastroUsuarios = new UsuariosCadastrados();
 	}
 
@@ -25,20 +25,22 @@ public class Administrador extends Usuario{
 			quantidadeUsuarios++;
 		}
 		else {
-			System.out.println("O usuário já se encontra cadastrado na plataforma. \n");
+			System.out.println("O usuario ja se encontra cadastrado na plataforma. \n");
 		}
 	}
 	
 	public void removerCadastro(Usuario usuario_remover) {
 		if (this.getCadastroUsuarios().getUsuariosCadastrados().isEmpty()) {
-			System.out.println("A plataforma não possui nenhum usuário cadastrado \n");
+			System.out.println("A plataforma nao possui nenhum usuario cadastrado \n");
 		}
 		else {
 			if (this.getCadastroUsuarios().getUsuariosCadastrados().contains(usuario_remover)) {
 				this.getCadastroUsuarios().getUsuariosCadastrados().remove(usuario_remover);
 				quantidadeUsuarios--;
+				usuario_remover.setStatus(false);
 			}
 		}
 	}
 	
 }
+
